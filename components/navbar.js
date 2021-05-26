a3e471b13c37038371b96086364ce6e76612df72
cesanag <gcesa00@gmail.com>
2021-05-26 12:29:19 +0200

    add web components navbar, link utili, blog

---
+++ components/navbar.js
@@ -0,0 +1,56 @@
+class Navbar extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.innerHTML = `
+        <!--Navbar-->
+        <div class="navbar">
+            <div class="navbar--logo">
+                W Zero Waste
+            </div>
+            <div class="navbar--menu">
+                <a href="#prodotti">
+                    Prodotti
+                </a>
+                <a href="#eventi">
+                    Eventi
+                </a>
+                <a href="#linkutili">
+                    Link Utili
+                </a>
+                <a href="blog/blog.html">
+                    Blog
+                </a>
+            </div>
+    
+            <div
+                id="navbar--menu-burger"
+                onClick="toggleDropdownMenu()"
+                class="navbar--menu-burger">
+            </div>
+    
+    
+            <div id="navbar--menu-dropdown" 
+                class="navbar--menu-dropdown navbar--menu-is-visible">
+                    <a href="#prodotti">
+                        Prodotti
+                    </a>
+                    <a href="#eventi">
+                        Eventi
+                    </a>
+                    <a href="#linkutili">
+                        Link Utili
+                    </a>
+                    <a href="blog/blog.html">
+                        Blog
+                    </a>
+            </div>
+    
+        </div>
+    <!--End Navbar-->
+    `
+    }
+}
+
+customElements.define('navbar-component', Navbar);
---
+++ js/script.js
@@ -0,0 +1,46 @@
+
+
+// MenuBurger
+function toggleDropdownMenu() {
+    let menuDropdown = document.querySelector("#navbar--menu-dropdown")
+    menuDropdown.classList.toggle("navbar--menu-is-visible")
+    menuDropdown.style.zIndex = "999"
+}
+
+
+// SwiperJs
+const swiper = new Swiper('.swiper-container', {
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
+
+// GoUpButton
+const goUpButton = document.querySelector("#button--go-up");
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        goUpButton.style.display = "block";
+    } else {
+        goUpButton.style.display = "none";
+    }
+}
+function goToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
